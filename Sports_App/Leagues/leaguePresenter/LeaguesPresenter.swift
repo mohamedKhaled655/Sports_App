5873a9548dc1b3be49d96978ba26f923a4d1f929
NohaAliGomaa <nohagomaa97@gmail.com>
2025-05-21 10:29:11 +0300

    add Search Bar

--- Sports_App/Leagues/leaguePresenter/LeaguesPresenter.swift
+++ Sports_App/Leagues/leaguePresenter/LeaguesPresenter.swift
@@ -17,18 +17,47 @@ class LeaguesPresenter {
         self.networkManager = networkManager
     }
     
-    func fetchLeagues(_ sportName: String){
-        guard let sportType =  SportsType(rawValue: sportName) else {
+    func fetchLeagues(_ sportName: String) {
+        guard let sportType = SportsType(rawValue: sportName) else {
             return
         }
+        
         let url = APIRouter.getLeagues(params: LeaguesRquestParams(sportType: sportType))
-        networkManager.fetch(url){ (response: LeagueResponse?) in
-            if let leagues = response?.result{
+        
+        networkManager.fetch(url) { (response: LeagueResponse?) in
+            if let leagues = response?.result {
+                self.leagues = leagues
                 self.view?.showLeagues(leagues)
-            }else {
+            } else {
                 self.view?.showError("Failed to fetch leagues.")
             }
-            
+        }
+    }
+    
+    func searchLeagues(with query: String) {
+        let trimmed = query.trimmingCharacters(in: .whitespacesAndNewlines).lowercased()
+
+        if trimmed.isEmpty {
+            view?.showLeagues(leagues)
+        } else {
+            let filtered = leagues.filter {
+                ($0.league_name ?? "").lowercased().contains(trimmed)
+            }.sorted {
+                let name1 = $0.league_name?.lowercased() ?? ""
+                let name2 = $1.league_name?.lowercased() ?? ""
+                let startsWith1 = name1.hasPrefix(trimmed)
+                let startsWith2 = name2.hasPrefix(trimmed)
+
+                if startsWith1 && !startsWith2 {
+                    return true
+                } else if !startsWith1 && startsWith2 {
+                    return false
+                } else {
+                    return name1 < name2
+                }
+            }
+
+            view?.showLeagues(filtered)
         }
     }
     
--- Sports_App/Leagues/view/LeagueViewController.swift
+++ Sports_App/Leagues/view/LeagueViewController.swift
@@ -8,16 +8,19 @@
 import UIKit
 import SDWebImage
 
-class LeagueViewController: UIViewController,UITableViewDataSource, UITableViewDelegate , LeaguesViewProtocol ,FavouriteCellProtocol{
+class LeagueViewController: UIViewController,UITableViewDataSource, UITableViewDelegate , UISearchBarDelegate , LeaguesViewProtocol ,FavouriteCellProtocol{
     
     
-
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     @IBOutlet weak var leaguesTable: UITableView!
     
     var selectedSportName: String?
     
     var leaguesPresenter:LeaguesPresenter?
     var leagues:[LeagueModel] = []
+    var filteredLeagues: [LeagueModel] = []
+    var isSearching = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,8 +28,10 @@ class LeagueViewController: UIViewController,UITableViewDataSource, UITableViewD
         
         leaguesTable.dataSource = self
         leaguesTable.delegate = self
+        searchBar.delegate = self
         let cellNib = UINib(nibName: "LeagueTableViewCell", bundle: nil)
         leaguesTable.register(cellNib, forCellReuseIdentifier: "LeagueCell")
+        searchBar.placeholder = "Search leagues..."
         
         
         leaguesPresenter = LeaguesPresenter(view: self, networkManager: NetworkManager())
@@ -35,12 +40,23 @@ class LeagueViewController: UIViewController,UITableViewDataSource, UITableViewD
 
      
     }
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        leaguesPresenter?.searchLeagues(with: searchText)
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        isSearching = false
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
+        searchBar.showsCancelButton = true
+        leaguesTable.reloadData()
+    }
     
     func showLeagues(_ leagues: [LeagueModel]) {
-        self.leagues = leagues
-        print("country_name: \(leagues[0].country_name)")
+        self.filteredLeagues = leagues
+        self.isSearching = true 
         DispatchQueue.main.async {
-           self.leaguesTable.reloadData()
+            self.leaguesTable.reloadData()
         }
     }
     
@@ -49,7 +65,7 @@ class LeagueViewController: UIViewController,UITableViewDataSource, UITableViewD
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        leagues.count
+        isSearching ? filteredLeagues.count : leagues.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -57,7 +73,7 @@ class LeagueViewController: UIViewController,UITableViewDataSource, UITableViewD
         
         cell.cellView.layer.cornerRadius = 16
         
-        let league = leagues[indexPath.row]
+        let league = isSearching ? filteredLeagues[indexPath.row] : leagues[indexPath.row]
         cell.leagueTitle.text = league.league_name
         
         cell.leagueImage.sd_setImage(with: URL(string: league.league_logo ?? ""), placeholderImage: UIImage(named: "fifa"))
@@ -79,8 +95,8 @@ class LeagueViewController: UIViewController,UITableViewDataSource, UITableViewD
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
        let detials = self.storyboard?.instantiateViewController(withIdentifier: "DetialsCollectionViewController") as! DetialsCollectionViewController
-        detials.sportName = selectedSportName
-        detials.leaugeId = leagues[indexPath.row].league_key
+        let league = isSearching ? filteredLeagues[indexPath.row] : leagues[indexPath.row]
+        detials.leaugeId = league.league_key
         navigationController?.pushViewController(detials, animated: true)
         
     }
