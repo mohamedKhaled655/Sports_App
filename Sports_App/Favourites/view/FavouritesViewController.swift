7d4dcc7ed5f516eee535505a292dec04f5b16c44
mohamedKhaled655 <111822676+mohamedKhaled655@users.noreply.github.com>
2025-05-22 01:35:44 +0300

    nav to details from fav and check connectivity

--- Sports_App/Favourites/view/FavouritesViewController.swift
+++ Sports_App/Favourites/view/FavouritesViewController.swift
@@ -6,12 +6,17 @@
 //
 
 import UIKit
+import Reachability
 
 class FavouritesViewController: UIViewController , UITableViewDelegate, UITableViewDataSource {
  
     
     @IBOutlet weak var favoritesTable: UITableView!
-    var favoriteLeagues: [League] = []
+    //var favoriteLeagues: [League] = []
+    var favoriteLeaguesByName: [String: [League]] = [:]
+    var leagueName: [String] = []
+    let reachability = try! Reachability()
+    var isConnected: Bool = false
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -23,30 +28,79 @@ class FavouritesViewController: UIViewController , UITableViewDelegate, UITableV
         favoritesTable.register(cellNib, forCellReuseIdentifier: "LeagueCell")
         
         
+        setupReachability()
+        
+    }
+    
+    func setupReachability() {
+        reachability.whenReachable = {  _ in
+            print("Network reachable")
+            self.isConnected = true
+        }
+        
+        reachability.whenUnreachable = {  _ in
+            print("Network unreachable")
+            self.isConnected = false
+        }
+        
+        do {
+            try reachability.startNotifier()
+            isConnected = reachability.connection != .unavailable
+        } catch {
+            print("Unable to start notifier")
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         loadFavorites()
     }
+    deinit {
+            reachability.stopNotifier()
+        }
     func loadFavorites() {
-        favoriteLeagues = LocalDBManager.shared.getAllLeaguesFromDB()
+        let favoriteLeagues = LocalDBManager.shared.getAllLeaguesFromDB()
+        
+        var tempDict: [String: [League]] = [:]
+        for league in favoriteLeagues {
+            let name = league.sportName ?? ""
+            if tempDict[name] != nil{
+                tempDict[name]?.append(league)
+            }else{
+                tempDict[name] = [league]
+            }
+            
+        }
+        
+        favoriteLeaguesByName = tempDict
+        leagueName = favoriteLeaguesByName.keys.sorted()
+        
         print("Loaded favorites: \(favoriteLeagues.count)")
         self.favoritesTable.reloadData()
         
         }
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+            return leagueName.count
+    }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return favoriteLeagues.count
+        let name = leagueName[section]
+        return favoriteLeaguesByName[name]?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return leagueName[section]
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let league = favoriteLeagues[indexPath.row]
+        //let league = favoriteLeagues[indexPath.row]
+        let name = leagueName[indexPath.section]
+        let league = favoriteLeaguesByName[name]?[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "LeagueCell", for: indexPath) as! LeagueTableViewCell
         
         cell.cellView.layer.cornerRadius = 16
-        cell.leagueTitle.text = league.leagueName
-        cell.leagueImage.sd_setImage(with: URL(string: league.leagueLogo ?? ""), placeholderImage: UIImage(named: "fifa"))
+        cell.leagueTitle.text = league?.leagueName
+        cell.leagueImage.sd_setImage(with: URL(string: league?.leagueLogo ?? ""), placeholderImage: UIImage(named: "fifa"))
         cell.delegate = nil
         
         cell.favBtn.isHidden = true
@@ -59,10 +113,11 @@ class FavouritesViewController: UIViewController , UITableViewDelegate, UITableV
     }
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            let leagueKey = favoriteLeagues[indexPath.row].leagueKey
+            let name = leagueName[indexPath.section]
+            let leagueKey = favoriteLeaguesByName[name]?[indexPath.row].leagueKey
             let alert = UIAlertController(
                     title: "Confirm Deletion",
-                    message: "Are you sure you want to remove \"\(favoriteLeagues[indexPath.row].leagueName ?? "")\" from favorites?",
+                    message: "Are you sure you want to remove \"\(favoriteLeaguesByName[name]?[indexPath.row].leagueName ?? "")\" from favorites?",
                     preferredStyle: .alert
                 )
                     
@@ -78,8 +133,28 @@ class FavouritesViewController: UIViewController , UITableViewDelegate, UITableV
         }
     }
     
-    
-    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        if isConnected {
+            let name = leagueName[indexPath.section]
+            let selectedSport = favoriteLeaguesByName[name]?[indexPath.row].sportName
+            if let vc = storyboard?.instantiateViewController(withIdentifier: "DetialsCollectionViewController") as? DetialsCollectionViewController {
+                vc.sportName = selectedSport
+                vc.leaugeId = favoriteLeaguesByName[name]?[indexPath.row].leagueKey
+                vc.hidesBottomBarWhenPushed = true
+                navigationController?.pushViewController(vc, animated: true)
+            }
+        } else {
+            let alert = UIAlertController(
+                title: "No Internet Connection",
+                message: "Please check your internet connection and try again.",
+                preferredStyle: .alert
+            )
+            alert.addAction(UIAlertAction(title: "OK", style: .default))
+            present(alert, animated: true)
+        }
+        
+    }
     
 
     /*
--- Sports_App/Team/view/TeamViewController.swift
+++ Sports_App/Team/view/TeamViewController.swift
@@ -15,7 +15,8 @@ class TeamViewController: UIViewController ,UITableViewDataSource, UITableViewDe
     var teamId: Int?
     var sportName: String?
     var teamPresenter:TeamPresenter?
-    var players:[Player] = []
+    var playersByType: [String: [Player]] = [:]
+    var playerTypes: [String] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         print("teamId \(teamId ?? 0)")
@@ -32,10 +33,25 @@ class TeamViewController: UIViewController ,UITableViewDataSource, UITableViewDe
         teamPresenter?.fetchTeamDetails(sportName,teamId)
     }
     func showLeagues(_ team: [Team]) {
-        self.players = team.first?.players ?? []
-       
+        let players = team.first?.players ?? []
+        
+        var tempDict: [String: [Player]] = [:]
+        
+        for player in players {
+            let type = player.player_type ?? ""
+            if tempDict[type] != nil{
+                tempDict[type]?.append(player)
+            }else{
+                tempDict[type] = [player]
+            }
+        }
+        
+        playersByType = tempDict
+        let customOrder = ["Goalkeepers", "Defenders", "Midfielders", "Forwards"]
+        playerTypes = customOrder.filter{playersByType.keys.contains($0) }
+        
         DispatchQueue.main.async {
-            print("players : \(self.players )")
+            print("players : \(self.playersByType )")
            self.playersTable.reloadData()
         }
     }
@@ -44,21 +60,29 @@ class TeamViewController: UIViewController ,UITableViewDataSource, UITableViewDe
         print("Error: \(message)")
     }
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+            return playerTypes.count
+    }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        players.count
+        let type = playerTypes[section]
+        return playersByType[type]?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return playerTypes[section]
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "PlayerCell", for: indexPath) as! PlayerTableViewCell
+        let type = playerTypes[indexPath.section]
+        let player = playersByType[type]?[indexPath.row]
         
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PlayerCell", for: indexPath) as! PlayerTableViewCell
         cell.viewCell.layer.cornerRadius = 16
         
-        let player = players[indexPath.row]
-        cell.playerName.text = player.player_name
-        cell.playerType.text = player.player_type
-        cell.playerNum.text = player.player_number
-        
-        cell.playerImage.sd_setImage(with: URL(string: player.player_image ?? ""), placeholderImage: UIImage(named: "fifa"))
+        cell.playerName.text = player?.player_name
+        cell.playerType.text = player?.player_type
+        cell.playerNum.text = player?.player_number
+        cell.playerImage.sd_setImage(with: URL(string: player?.player_image ?? ""), placeholderImage: UIImage(named: "person"))
         
         return cell
     }
